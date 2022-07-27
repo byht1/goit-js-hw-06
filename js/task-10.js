49c4068c1f3a4e58b257388a247a3fd80ae50fe7
byht1 <vitalikbyht1@gmail.com>
2022-07-27 21:32:06 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,12 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function randoms() {
-  return ('000000000' + Math.random().toString(36).substr(2, 9)).slice(-9);
-}
+// function randoms() {
+//   return ('000000000' + Math.random().toString(36).substr(2, 9)).slice(-9);
+// }
 
 const rest = {
-  interface: document.querySelector('#controls'),
+  // interface: document.querySelector('#controls'),
   startButton: document.querySelector('[data-create]'),
   deleteButton: document.querySelector('[data-destroy]'),
   conteinet: document.querySelector('#boxes'),
@@ -15,16 +15,16 @@ const rest = {
 let array = [];
 
 const construcror = event => {
-  const a = document.getElementsByTagName('input')[0].value;
+  const inputValue = document.getElementsByTagName('input')[0].value;
   const element = document.querySelectorAll('.activ');
   let w = 30;
   if (element.length > 0) {
     w = 30 + element.length * 10;
   }
 
-  for (let i = 1; i <= a; i += 1) {
+  for (let i = 1; i <= inputValue; i += 1) {
     const element = document.createElement('div');
-    element.id = `${randoms()}`;
+    // element.id = `${randoms()}`;
     element.style.width = `${w + i * 10}px`;
     element.style.height = `${w + i * 10}px`;
     element.style.backgroundColor = `${getRandomHexColor()}`;
@@ -38,6 +38,7 @@ const construcror = event => {
 
 const construcrorDelete = () => {
   const a = rest.conteinet;
+  console.log('🚀 ~ a', a);
   while (a.firstChild) {
     a.removeChild(a.firstChild);
   }
